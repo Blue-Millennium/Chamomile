5de0a7d11c6e9d1ea04f30c73fb7a6598d4d01ad
Suisuroru <qwertyuiop14077@qq.com>
2024-08-15 23:40:53 +0800

    更正一处细微错误

--- src/main/java/xd/suka/module/impl/Reporter.java
+++ src/main/java/xd/suka/module/impl/Reporter.java
@@ -78,7 +78,7 @@ public class Reporter extends Module implements Listener {
 
         MessageChainBuilder builder = new MessageChainBuilder();
         String ip = event.getAddress().getHostAddress();
-        builder.append(event.getName()).append(" was logging in").append(servername).append('\n').append("IP: ").append(ip);
+        builder.append(event.getName()).append(" was logging in ").append(servername).append('\n').append("IP: ").append(ip);
         IpLocationResponse response = IpinfoUtil.getIpinfoCN(ip);
 
         if (response != null) {
