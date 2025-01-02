d6176fbc21f8b252fb449ed6c2a4b43cfcf75116
Suisuroru <qwertyuiop14077@qq.com>
2025-01-02 13:36:59 +0800

    new: disable damage in SPECTATOR

--- src/main/java/fun/suya/suisuroru/module/impl/DamageDisableListener.java
+++ src/main/java/fun/suya/suisuroru/module/impl/DamageDisableListener.java
@@ -1,5 +1,6 @@
 package fun.suya.suisuroru.module.impl;
 
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,7 +11,7 @@ public class DamageDisableListener implements Listener {
     @EventHandler
     public void onEntityDamage(EntityDamageEvent event) {
         if (event.getEntity() instanceof Player player) {
-            if (player.getGameMode().equals(org.bukkit.GameMode.CREATIVE)) {
+            if (player.getGameMode().equals(org.bukkit.GameMode.CREATIVE) || player.getGameMode().equals(GameMode.SPECTATOR)) {
                 event.setCancelled(true);
             }
         }
