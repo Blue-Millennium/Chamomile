3ffe8b77d71117d3dffaf02bd764671d963099e5
Suisuroru <qwertyuiop14077@qq.com>
2025-01-02 12:59:19 +0800

    fi: kill logic

--- src/main/java/fun/suya/suisuroru/commands/execute/vanilla/Kill.java
+++ src/main/java/fun/suya/suisuroru/commands/execute/vanilla/Kill.java
@@ -1,5 +1,6 @@
 package fun.suya.suisuroru.commands.execute.vanilla;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,6 +9,31 @@ import org.jetbrains.annotations.NotNull;
 public class Kill implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (!sender.isOp()) {
+            sender.sendMessage("您没有权限这么做");
+            return false;
+        }
+        if (args[0].startsWith("@e")) {
+            if (args[0].equals("@e")) {
+                Command.broadcastCommandMessage(sender, "§c拒绝执行清除全部实体，请指定具体实体类型");
+                return true;
+            } else if (args[0].contains("type=!")) {
+                Command.broadcastCommandMessage(sender, "§c拒绝执行清除多类型全部实体，请指定具体实体类型");
+                return true;
+            } else {
+                Command.broadcastCommandMessage(sender, "§c拒绝执行清除全部实体，请指定具体实体类型");
+            }
+        } else if (args[0].equals("@a")) {
+            Command.broadcastCommandMessage(sender, "§c拒绝执行清除全部玩家，请选择其他具体实体类型");
+            return true;
+        } else if (args[0].equals("@r")) {
+            Command.broadcastCommandMessage(sender, "§c拒绝执行清除随机玩家，请选择其他具体实体类型");
+            return true;
+        } else if (args[0].equals("item")) {
+            return Bukkit.dispatchCommand(sender, "minecraft:kill @e[type=item]");
+        } else {
+            return Bukkit.dispatchCommand(sender, "minecraft:kill " + String.join(" ", args));
+        }
         return false;
     }
 }
