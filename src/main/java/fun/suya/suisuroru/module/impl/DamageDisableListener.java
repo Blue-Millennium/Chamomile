012498af1463d212e742d6de3d47e3841b5a755e
Suisuroru <qwertyuiop14077@qq.com>
2025-01-02 13:36:09 +0800

    new: disable damage in creative

---
+++ src/main/java/fun/suya/suisuroru/module/impl/DamageDisableListener.java
@@ -0,0 +1,18 @@
+package fun.suya.suisuroru.module.impl;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class DamageDisableListener implements Listener {
+
+    @EventHandler
+    public void onEntityDamage(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            if (player.getGameMode().equals(org.bukkit.GameMode.CREATIVE)) {
+                event.setCancelled(true);
+            }
+        }
+    }
+}
