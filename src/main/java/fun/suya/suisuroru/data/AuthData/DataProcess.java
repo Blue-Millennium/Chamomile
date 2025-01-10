ffef13ef47f95d82471074b1c0b1a87bea4edfdb
Suisuroru <qwertyuiop14077@qq.com>
2025-01-10 14:59:48 +0800

    refactor(data): convert AuthData, PlayerData, and PlayerRecord classes to record classes

--- src/main/java/fun/suya/suisuroru/data/AuthData/DataProcess.java
+++ src/main/java/fun/suya/suisuroru/data/AuthData/DataProcess.java
@@ -8,14 +8,14 @@ public class DataProcess {
         Gson gson = new Gson();
         AuthData authData = gson.fromJson(jsonData, AuthData.class);
         StringBuilder result = new StringBuilder();
-        result.append("玩家名称: ").append(authData.getPlayerData().getPlayerName()).append("\n");
-        result.append("玩家UUID: ").append(authData.getPlayerData().getPlayerUuid()).append("\n");
-        result.append("首次加入时间: ").append(authData.getFirstJoin()).append("\n");
-        result.append("最后加入时间: ").append(authData.getLastJoin()).append("\n");
-        result.append("QQ号码: ").append(authData.getQqNumber()).append("\n");
-        result.append("绑定时间: ").append(authData.getLinkedTime()).append("\n");
-        result.append("首次加入IP: ").append(authData.getFirstJoinIp()).append("\n");
-        result.append("最后加入IP: ").append(authData.getLastJoinIp()).append("\n");
+        result.append("玩家名称: ").append(authData.playerData().playerName()).append("\n");
+        result.append("玩家UUID: ").append(authData.playerData().playerUuid()).append("\n");
+        result.append("首次加入时间: ").append(authData.firstJoin()).append("\n");
+        result.append("最后加入时间: ").append(authData.lastJoin()).append("\n");
+        result.append("QQ号码: ").append(authData.qqNumber()).append("\n");
+        result.append("绑定时间: ").append(authData.linkedTime()).append("\n");
+        result.append("首次加入IP: ").append(authData.firstJoinIp()).append("\n");
+        result.append("最后加入IP: ").append(authData.lastJoinIp()).append("\n");
 
         return result.toString();
     }
