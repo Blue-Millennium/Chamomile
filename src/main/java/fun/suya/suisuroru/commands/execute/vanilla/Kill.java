f60a9ae71377b93883b44aeb3a70ed5d0ac592fb
Suisuroru <qwertyuiop14077@qq.com>
2025-01-16 00:06:52 +0800

    fix: fix bugs in kill command - 2

--- src/main/java/fun/suya/suisuroru/commands/execute/vanilla/Kill.java
+++ src/main/java/fun/suya/suisuroru/commands/execute/vanilla/Kill.java
@@ -13,26 +13,30 @@ public class Kill implements CommandExecutor {
             sender.sendMessage("您没有权限这么做");
             return false;
         }
-        if (args[0].startsWith("@e")) {
-            if (args[1].isEmpty()) {
-                Command.broadcastCommandMessage(sender, "§c拒绝执行清除全部实体，请指定具体实体类型");
-                return true;
-            } else if (args[0].contains("type=!")) {
-                Command.broadcastCommandMessage(sender, "§c拒绝执行清除多类型全部实体，请指定具体实体类型");
-                return true;
-            } else {
-                Command.broadcastCommandMessage(sender, "§c拒绝执行清除全部实体，请指定具体实体类型");
+        switch (args[0]) {
+            case "@e" -> {
+                if (args[1].isEmpty()) {
+                    Command.broadcastCommandMessage(sender, "§c拒绝执行清除全部实体，请指定具体实体类型");
+                    return true;
+                } else if (args[1].contains("type=!")) {
+                    Command.broadcastCommandMessage(sender, "§c拒绝执行清除多类型全部实体，请指定具体实体类型");
+                    return true;
+                }
             }
-        } else if (args[0].equals("@a")) {
-            Command.broadcastCommandMessage(sender, "§c拒绝执行清除全部玩家，请选择其他具体实体类型");
-            return true;
-        } else if (args[0].equals("@r")) {
-            Command.broadcastCommandMessage(sender, "§c拒绝执行清除随机玩家，请选择其他具体实体类型");
-            return true;
-        } else if (args[0].equals("items")) {
-            return Bukkit.dispatchCommand(sender, "minecraft:kill @e[type=item]");
-        } else {
-            return Bukkit.dispatchCommand(sender, "minecraft:kill " + String.join(" ", args));
+            case "@a" -> {
+                Command.broadcastCommandMessage(sender, "§c拒绝执行清除全部玩家，请选择其他具体实体类型");
+                return true;
+            }
+            case "@r" -> {
+                Command.broadcastCommandMessage(sender, "§c拒绝执行清除随机玩家，请选择其他具体实体类型");
+                return true;
+            }
+            case "items" -> {
+                return Bukkit.dispatchCommand(sender, "minecraft:kill @e[type=item]");
+            }
+            default -> {
+                return Bukkit.dispatchCommand(sender, "minecraft:kill " + String.join(" ", args));
+            }
         }
         return false;
     }
