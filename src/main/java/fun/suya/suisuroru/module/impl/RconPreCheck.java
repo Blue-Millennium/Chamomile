c21d1f7ea28db817db460156f47ac514c05aded0
Suisuroru <qwertyuiop14077@qq.com>
2025-02-03 21:49:17 +0800

    [skip]refactor(rcon): refactoring the execution logic of RCON command

--- src/main/java/fun/suya/suisuroru/module/impl/RconPreCheck.java
+++ src/main/java/fun/suya/suisuroru/module/impl/RconPreCheck.java
@@ -78,7 +78,7 @@ public class RconPreCheck extends Module implements Listener {
                         return;
                     }
                 }
-                String[] result = executeRconCommand(Config.RconIP, Config.RconPort, command);
+                String[] result = executeRconCommand(Config.RconIP, Config.RconPort, Config.RconPassword, command);
                 handleConsoleResult(result, event);
             }
         });
