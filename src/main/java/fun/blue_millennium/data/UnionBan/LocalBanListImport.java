48a0cf3ecdc16546bce3480927ace5cbfd944e9b
Suisuroru <qwertyuiop14077@qq.com>
2025-02-14 18:47:28 +0800

    refactor(UnionBan): optimize the import logic of the local ban list

--- src/main/java/fun/blue_millennium/data/UnionBan/LocalBanListImport.java
+++ src/main/java/fun/blue_millennium/data/UnionBan/LocalBanListImport.java
@@ -8,32 +8,30 @@ import org.bukkit.Bukkit;
 import org.bukkit.ban.ProfileBanList;
 import org.bukkit.profile.PlayerProfile;
 
-import java.util.UUID;
-
 import static fun.blue_millennium.Main.LOGGER;
 import static fun.blue_millennium.data.UnionBan.OnlineProcess.OnlinePush.reportRemoteBanList;
 
 public class LocalBanListImport {
     public static void importBanList() {
-        try{
+        try {
             ProfileBanList banlist = Bukkit.getBanList(BanList.Type.PROFILE);
-        UnionBanDataGet dg = new UnionBanDataGet();
-        for (BanEntry<PlayerProfile> banEntry : banlist.getEntries()) {
-            UnionBanData data = dg.getUnionBanData(banEntry.getBanTarget().getUniqueId());
-            if (data == null) {
-                data = new UnionBanData();
-                data.playerName = banEntry.getBanTarget().getName();
-                data.playerUuid = banEntry.getBanTarget().getUniqueId();
-                data.time = banEntry.getExpiration().getTime();
-                data.reason = banEntry.getReason();
-                data.sourceServer = Config.ServerName;
-                data.localTag = true;
-                if (reportRemoteBanList(data)) {
-                    data.reportTag = true;
+            UnionBanDataGet dg = new UnionBanDataGet();
+            for (BanEntry<PlayerProfile> banEntry : banlist.getEntries()) {
+                UnionBanData data = dg.getUnionBanData(banEntry.getBanTarget().getUniqueId());
+                if (data == null) {
+                    data = new UnionBanData();
+                    data.playerName = banEntry.getBanTarget().getName();
+                    data.playerUuid = banEntry.getBanTarget().getUniqueId();
+                    data.time = banEntry.getExpiration().getTime();
+                    data.reason = banEntry.getReason();
+                    data.sourceServer = Config.ServerName;
+                    data.localTag = true;
+                    if (reportRemoteBanList(data)) {
+                        data.reportTag = true;
+                    }
+                    dg.setPlayerData(data.playerUuid, data);
                 }
-                dg.setPlayerData(data.playerUuid, data);
             }
-        }
         } catch (Exception e) {
             LOGGER.warning("导入本地封禁列表失败: " + e.getMessage());
         }
