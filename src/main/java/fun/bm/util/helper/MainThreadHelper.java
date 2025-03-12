548a234a19f371b7120b794a806034f1f1b7e4e7
Suisuroru <qwertyuiop14077@qq.com>
2025-03-13 00:09:31 +0800

    refactor: refactoring the MainThreadHelper class

--- src/main/java/fun/bm/util/helper/MainThreadHelper.java
+++ src/main/java/fun/bm/util/helper/MainThreadHelper.java
@@ -1,6 +1,5 @@
 package fun.bm.util.helper;
 
-import fun.bm.Chamomile;
 import fun.bm.config.Config;
 import fun.bm.config.ConfigManager;
 import net.mamoe.mirai.event.GlobalEventChannel;
@@ -10,60 +9,61 @@ import top.mrxiaom.overflow.BotBuilder;
 import java.io.File;
 import java.io.IOException;
 
+import static fun.bm.Chamomile.*;
+
 public class MainThreadHelper extends JavaPlugin {
     public static void Boot_QQBot() {
-        if (Chamomile.BOT != null) {
-            Chamomile.BOT.close();
+        if (BOT != null) {
+            BOT.close();
         }
         if (Config.QQRobotEnabled) {
-            Chamomile.BOT = BotBuilder.positive(Config.BotWsUrl).token(Config.BotWsToken).connect(); // 连接 LLOneBot
-            Chamomile.eventChannel = GlobalEventChannel.INSTANCE;
-            if (Chamomile.BOT == null) {
+            BOT = BotBuilder.positive(Config.BotWsUrl).token(Config.BotWsToken).connect(); // 连接 LLOneBot
+            eventChannel = GlobalEventChannel.INSTANCE;
+            if (BOT == null) {
                 Config.QQRobotEnabled = false;
                 ConfigManager manager = new ConfigManager();
                 manager.save();
-                Chamomile.LOGGER.warning("Failed to get bot instance");
+                LOGGER.warning("Failed to get bot instance");
             }
         } else {
-            Chamomile.LOGGER.info("QQ Robot has been disabled in this running regin.");
+            LOGGER.info("QQ Robot has been disabled in this running regin.");
         }
     }
 
     public static void SetupDirectories() {
-        if (!Chamomile.BASE_DIR.exists()) {
-            for (File file : Chamomile.OLD_BASE_DIR) {
+        if (!BASE_DIR.exists()) {
+            for (File file : OLD_BASE_DIR) {
                 if (file.exists()) {
                     try {
-                        DirectoryExecutor.copyDirectory(file, Chamomile.BASE_DIR);
-                        Chamomile.LOGGER.info("复制配置文件完成");
+                        DirectoryExecutor.copyDirectory(file, BASE_DIR);
+                        LOGGER.info("复制配置文件完成");
                         DirectoryExecutor.deleteDirectory(file);
-                        Chamomile.LOGGER.info("删除旧配置文件完成");
+                        LOGGER.info("删除旧配置文件完成");
                     } catch (IOException e) {
-                        Chamomile.LOGGER.warning("复制配置文件时出现异常: " + e.getMessage());
+                        LOGGER.warning("复制配置文件时出现异常: " + e.getMessage());
                     }
                 }
             }
-            if (!Chamomile.BASE_DIR.mkdir()) {
-                Chamomile.LOGGER.warning("Failed to create directory: " + Chamomile.BASE_DIR.getAbsolutePath());
+            if (!BASE_DIR.mkdir()) {
+                LOGGER.warning("Failed to create directory: " + BASE_DIR.getAbsolutePath());
             }
         }
-        if (!Chamomile.DATA_FILE.exists()) {
+        if (!DATA_FILE.exists()) {
             try {
-                if (!Chamomile.DATA_FILE.createNewFile()) {
-                    Chamomile.LOGGER.warning("Failed to create data file");
+                if (!DATA_FILE.createNewFile()) {
+                    LOGGER.warning("Failed to create data file");
                 }
             } catch (Exception e) {
-                Chamomile.LOGGER.warning("Failed to create data file " + e.getMessage());
+                LOGGER.warning("Failed to create data file " + e.getMessage());
             }
         }
-
-        if (!Chamomile.UNION_BAN_DATA_FILE.exists()) {
+        if (!UNION_BAN_DATA_FILE.exists()) {
             try {
-                if (!Chamomile.UNION_BAN_DATA_FILE.createNewFile()) {
-                    Chamomile.LOGGER.warning("Failed to create unionban data file");
+                if (!UNION_BAN_DATA_FILE.createNewFile()) {
+                    LOGGER.warning("Failed to create unionban data file");
                 }
             } catch (Exception e) {
-                Chamomile.LOGGER.warning("Failed to create unionban data file " + e.getMessage());
+                LOGGER.warning("Failed to create unionban data file " + e.getMessage());
             }
         }
     }
