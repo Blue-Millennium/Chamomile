52f7fd2e015fbaa162076376daf4766543f43f10
Helvetica Volubi <suisuroru@blue-millennium.fun>
2025-04-05 00:48:09 +0800

    feat(command): update ReportQuery.java

--- src/main/java/fun/bm/command/main/executor/extra/sub/report/ReportQuery.java
+++ src/main/java/fun/bm/command/main/executor/extra/sub/report/ReportQuery.java
@@ -41,9 +41,7 @@ public class ReportQuery extends Command.ExecutorE {
 
     public boolean executorMain(@NotNull CommandSender sender, @NotNull org.bukkit.command.Command command, @NotNull String label, @NotNull String[] args) {
         // 检查发送者是否具有OP权限
-        if (checkNotOperator(sender)) {
-        } else {
-
+        if (!checkNotOperator(sender)) {
             try {
                 String QueryData = query();
                 sender.sendMessage(QueryData);
