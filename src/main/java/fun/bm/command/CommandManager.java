256b57be29fbdd4a2b690018087f63a8aa84ad16
Helvetica Volubi <suisuroru@blue-millennium.fun>
2025-05-07 01:00:16 +0800

    fix(command): added exception handling in CommandManager

--- src/main/java/fun/bm/command/CommandManager.java
+++ src/main/java/fun/bm/command/CommandManager.java
@@ -7,16 +7,20 @@ import org.bukkit.command.TabCompleter;
 import static fun.bm.util.helper.ClassLoader.loadClazz;
 
 public class CommandManager {
-    public static void registerCommands() {
+    public void registerCommands() {
         for (Object command : loadClazz("fun.bm.command.main")) {
-            String commandName;
-            commandName = ((Command.GlobalE) command).getCommandName();
-            if (commandName != null) {
-                if (command instanceof CommandExecutor) {
-                    MainEnv.INSTANCE.getCommand(commandName).setExecutor((CommandExecutor) command);
-                } else if (command instanceof TabCompleter) {
-                    MainEnv.INSTANCE.getCommand(commandName).setTabCompleter((TabCompleter) command);
+            try {
+                String commandName;
+                commandName = ((Command.GlobalE) command).getCommandName();
+                if (commandName != null) {
+                    if (command instanceof CommandExecutor) {
+                        MainEnv.INSTANCE.getCommand(commandName).setExecutor((CommandExecutor) command);
+                    } else if (command instanceof TabCompleter) {
+                        MainEnv.INSTANCE.getCommand(commandName).setTabCompleter((TabCompleter) command);
+                    }
                 }
+            } catch (Exception e) {
+                MainEnv.INSTANCE.getLogger().warning(e.getMessage());
             }
         }
     }
